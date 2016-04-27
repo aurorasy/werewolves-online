3b223566412ab45dcd15b05a4ec698f4516592e1
aurorasy <shenyuanbupt@126.com>
2016-04-27 11:43:14 +0200

    heroku try

---
+++ licode/erizo/build/erizo/CMakeFiles/erizo.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/aurora/licode/erizo/src/erizo/dtls/bf_dwrap.c" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/dtls/bf_dwrap.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aurora/licode/erizo/src/erizo/DtlsTransport.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/DtlsTransport.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/NiceConnection.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/NiceConnection.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/OneToManyProcessor.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/OneToManyProcessor.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/SdpInfo.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/SdpInfo.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/SrtpChannel.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/SrtpChannel.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/Stats.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/Stats.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/StringUtil.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/StringUtil.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/WebRtcConnection.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/WebRtcConnection.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/dtls/DtlsClient.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/dtls/DtlsClient.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/dtls/DtlsFactory.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/dtls/DtlsFactory.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/dtls/DtlsSocket.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/dtls/DtlsSocket.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/dtls/DtlsTimer.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/dtls/DtlsTimer.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/dtls/OpenSSLInit.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/dtls/OpenSSLInit.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/media/ExternalInput.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/media/ExternalInput.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/media/ExternalOutput.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/media/ExternalOutput.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/media/MediaProcessor.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/media/MediaProcessor.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/media/OneToManyTranscoder.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/media/OneToManyTranscoder.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/media/codecs/AudioCodec.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/media/codecs/AudioCodec.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/media/codecs/VideoCodec.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/media/codecs/VideoCodec.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/media/mixers/VideoMixer.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/media/mixers/VideoMixer.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/media/mixers/VideoUtils.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/media/mixers/VideoUtils.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/RtcpProcessor.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/RtcpProcessor.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/RtpPacketQueue.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/RtpPacketQueue.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/RtpSink.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/RtpSink.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/RtpSource.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/RtpSource.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/RtpVP8Fragmenter.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/RtpVP8Fragmenter.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/RtpVP8Parser.cpp" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/RtpVP8Parser.cpp.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/webrtc/fec_receiver_impl.cc" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/webrtc/fec_receiver_impl.cc.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/webrtc/forward_error_correction.cc" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/webrtc/forward_error_correction.cc.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/webrtc/forward_error_correction_internal.cc" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/webrtc/forward_error_correction_internal.cc.o"
+  "/home/aurora/licode/erizo/src/erizo/rtp/webrtc/rtp_utility.cc" "/home/aurora/licode/erizo/build/erizo/CMakeFiles/erizo.dir/rtp/webrtc/rtp_utility.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/aurora/licode/erizo/src/erizo"
+  "/home/aurora/licode/erizo/src/../../build/libdeps/build/include"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ licode/erizoAPI/build/Release/.deps/Release/obj.target/addon/WebRtcConnection.o.d
@@ -0,0 +1,157 @@
+cmd_Release/obj.target/addon/WebRtcConnection.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/aurora/.node-gyp/0.10.37/include/node -I/home/aurora/.node-gyp/0.10.37/src -I/home/aurora/.node-gyp/0.10.37/deps/uv/include -I/home/aurora/.node-gyp/0.10.37/deps/v8/include -I/home/aurora/licode/scripts/./../erizo/src/erizo -I/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -D__STDC_CONSTANT_MACROS -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-exceptions -Wall -O3 -g -std=c++0x -fexceptions -MMD -MF ./Release/.deps/Release/obj.target/addon/WebRtcConnection.o.d.raw   -c -o Release/obj.target/addon/WebRtcConnection.o ../WebRtcConnection.cc
+Release/obj.target/addon/WebRtcConnection.o: ../WebRtcConnection.cc \
+ ../WebRtcConnection.h /home/aurora/.node-gyp/0.10.37/src/node.h \
+ /home/aurora/.node-gyp/0.10.37/deps/uv/include/uv.h \
+ /home/aurora/.node-gyp/0.10.37/deps/uv/include/uv-private/uv-unix.h \
+ /home/aurora/.node-gyp/0.10.37/deps/uv/include/uv-private/ngx-queue.h \
+ /home/aurora/.node-gyp/0.10.37/deps/uv/include/uv-private/uv-linux.h \
+ /home/aurora/.node-gyp/0.10.37/deps/v8/include/v8.h \
+ /home/aurora/.node-gyp/0.10.37/deps/v8/include/v8stdint.h \
+ /home/aurora/.node-gyp/0.10.37/src/node_object_wrap.h \
+ /home/aurora/.node-gyp/0.10.37/src/node.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/WebRtcConnection.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/logger.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/SdpInfo.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/MediaDefinitions.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/Transport.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/NiceConnection.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/Stats.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtpHeaders.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/fec_receiver_impl.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/forward_error_correction.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/scoped_refptr.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/rtp_utility.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtcpProcessor.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/logger.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/MediaDefinitions.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/SdpInfo.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtpHeaders.h \
+ ../MediaDefinitions.h ../OneToManyProcessor.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/OneToManyProcessor.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/media/ExternalOutput.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/media/../MediaDefinitions.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtpPacketQueue.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/fec_receiver_impl.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/media/MediaProcessor.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtpVP8Parser.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/media/codecs/Codecs.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/media/codecs/VideoCodec.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/media/codecs/Codecs.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/avutil.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/error.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/rational.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/attributes.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/version.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/macros.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavcodec/avcodec.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/samplefmt.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/avutil.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/attributes.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/buffer.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/cpu.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/dict.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/frame.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/buffer.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/dict.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/samplefmt.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/pixfmt.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/avconfig.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/old_pix_fmts.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/log.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/pixfmt.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/rational.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavcodec/version.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/version.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/mem.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavformat/avformat.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavformat/avio.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/common.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/common.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/mem.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavformat/version.h \
+ ../ExternalInput.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/media/ExternalInput.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/mathematics.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/intfloat.h \
+ /home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/time.h \
+ ../ExternalOutput.h \
+ /home/aurora/licode/scripts/./../erizo/src/erizo/media/ExternalOutput.h
+../WebRtcConnection.cc:
+../WebRtcConnection.h:
+/home/aurora/.node-gyp/0.10.37/src/node.h:
+/home/aurora/.node-gyp/0.10.37/deps/uv/include/uv.h:
+/home/aurora/.node-gyp/0.10.37/deps/uv/include/uv-private/uv-unix.h:
+/home/aurora/.node-gyp/0.10.37/deps/uv/include/uv-private/ngx-queue.h:
+/home/aurora/.node-gyp/0.10.37/deps/uv/include/uv-private/uv-linux.h:
+/home/aurora/.node-gyp/0.10.37/deps/v8/include/v8.h:
+/home/aurora/.node-gyp/0.10.37/deps/v8/include/v8stdint.h:
+/home/aurora/.node-gyp/0.10.37/src/node_object_wrap.h:
+/home/aurora/.node-gyp/0.10.37/src/node.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/WebRtcConnection.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/logger.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/SdpInfo.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/MediaDefinitions.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/Transport.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/NiceConnection.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/Stats.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtpHeaders.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/fec_receiver_impl.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/forward_error_correction.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/scoped_refptr.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/rtp_utility.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtcpProcessor.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/logger.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/MediaDefinitions.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/SdpInfo.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtpHeaders.h:
+../MediaDefinitions.h:
+../OneToManyProcessor.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/OneToManyProcessor.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/media/ExternalOutput.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/media/../MediaDefinitions.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtpPacketQueue.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/webrtc/fec_receiver_impl.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/media/MediaProcessor.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/rtp/RtpVP8Parser.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/media/codecs/Codecs.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/media/codecs/VideoCodec.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/media/codecs/Codecs.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/avutil.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/error.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/rational.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/attributes.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/version.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/macros.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavcodec/avcodec.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/samplefmt.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/avutil.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/attributes.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/buffer.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/cpu.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/dict.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/frame.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/buffer.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/dict.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/samplefmt.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/pixfmt.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/avconfig.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/old_pix_fmts.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/log.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/pixfmt.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/rational.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavcodec/version.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/version.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/mem.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavformat/avformat.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavformat/avio.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/common.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/common.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/mem.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavformat/version.h:
+../ExternalInput.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/media/ExternalInput.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/mathematics.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/intfloat.h:
+/home/aurora/licode/scripts/./../erizo/../build/libdeps/build/include/libavutil/time.h:
+../ExternalOutput.h:
+/home/aurora/licode/scripts/./../erizo/src/erizo/media/ExternalOutput.h:
